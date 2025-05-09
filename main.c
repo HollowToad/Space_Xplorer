a9beb3c1332c32375db97f07f57d22a51ff52a6b
Cowan McVeigh <cowan2.mcveigh@live.uwe.ac.uk>
2025-05-09 13:56:54 +0100

    Changed the nature of the array by adding variables

--- main.c
+++ main.c
@@ -5,29 +5,37 @@ Date 09/05/2025
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-#define R 25 // Defining Rows to be 3 along
-#define C 25 // Defining Columns to be 3 Down
 
-void SpaceFieldArray(int Arr[R][C], int max) // Function to fill arrays that use R and C with random 1s and 0s
-{
-    for (int i = 0; i < R; i++) { // for loop to generate the rows
-        for (int j = 0; j < C; j++) { // Nested for loop to generate the column
-            Arr[i][j] = (rand() % max); //inputs random numbers into the array
-        }
-    }
-}
+#define R 15 // Defining Rows to be 15 along
+#define C 15 // Defining Columns to be 15 Down
 
+#define Asteriod '*'
+#define Fuel '!'
+#define Empty ' '
 
-int main(){
-    int Arr[R][C];
-    SpaceFieldArray(Arr, 2);
+void SpaceFieldArray(int Arr[R][C]) {// This function decides how to populate the array with desired variables
+    srand(time(NULL));
     for (int i = 0; i < R; i++) {
         for (int j = 0; j < C; j++) {
-            printf("%d ", Arr[i][j]);
+            int x = rand() % 10;
+            if (x < 2) Arr[i][j] = Asteriod; // This makes every 2 in 10 spaces an asteroid (20%)
+            else if (x < 5) Arr[i][j] = Fuel; // This makes every 3 in 10 spaces a fuel (30%)
+            else Arr[i][j] = Empty; // This makes every 5 in 10 spaces an empty space (50%)
         }
-        printf("\n");
     }
 
-    return 0;
+}
+
+void FormSpace(char Arr[R][C], int moveRow, int moveCol) {// This function prints the game grid
+    for (int i = 0; i < R; i++) {
+        for (int j = 0; j < C; j++) {
+            if (i == moveRow && j == moveCol) {
+                printf("0 ");// '0' is marking the player's position in the array, This line shows where they are
+            }else {
+                printf("%c ", Arr[i][j]); // This line displays what the prior function placed in this cell
+        }
+        }
+        printf("\n"); //It then goes to the next row and displays the cells
+    }
 }
 
